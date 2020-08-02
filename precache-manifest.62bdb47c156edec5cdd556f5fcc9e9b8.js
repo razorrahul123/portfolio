878807205630d08a093c8efe68424a1f438fa4b5
razorrahul123 <rahul.kr.sushant@gmail.com>
2020-08-02 10:19:32 +0530

    Updates

---
+++ precache-manifest.62bdb47c156edec5cdd556f5fcc9e9b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41ec12eba4ca6b22b5a4477200da7e0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3f7dc1524d3f62f08684",
+    "url": "/portfolio/static/css/main.44f4330f.chunk.css"
+  },
+  {
+    "revision": "76b83ed43db6fbcac5ad",
+    "url": "/portfolio/static/js/2.4f7ed88f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.4f7ed88f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f7dc1524d3f62f08684",
+    "url": "/portfolio/static/js/main.d72358c7.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "6eb5cd8c0cb7b18d79a08a304e83a55d",
+    "url": "/portfolio/static/media/photo.6eb5cd8c.jpg"
+  },
+  {
+    "revision": "62595d43b5e230fd4483d4a25beb150f",
+    "url": "/portfolio/static/media/profile-pic.62595d43.jpg"
+  },
+  {
+    "revision": "2c794e58ad91a9450fe602e989c02267",
+    "url": "/portfolio/static/media/resumeSushant.2c794e58.pdf"
+  }
+]);
