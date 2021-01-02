fdad25df66f113c612d62946e9f00f05f72a5fee
Sushant Kumar Singh <sushant@urbanpiper.com>
2021-01-02 21:56:02 +0530

    some more changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import About from "./component/About";
-// import Info from "./component/Info";
 import Home from "./components/Home";
-// import NotFound from "./pages/NotFound";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="app">
-      <Router>
+    <div className="App">
+      <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route path="/" exact component={Home} />
-
-          {/* <Route path="/person/:id" component={About} /> */}
-
-          {/* <Route component={NotFound} /> */}
         </Switch>
       </Router>
     </div>
