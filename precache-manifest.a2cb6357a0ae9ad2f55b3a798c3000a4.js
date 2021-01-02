5ba41c17a531641e7313cb6d200fb82c001ffb18
Sushant Kumar Singh <sushant@urbanpiper.com>
2021-01-02 21:51:54 +0530

    Updates

---
+++ precache-manifest.a2cb6357a0ae9ad2f55b3a798c3000a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0cfb5bd4ac316735a142755cc337280",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "729437c7efd2e8b4c78e",
+    "url": "/portfolio/static/css/main.44f4330f.chunk.css"
+  },
+  {
+    "revision": "76b83ed43db6fbcac5ad",
+    "url": "/portfolio/static/js/2.4f7ed88f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.4f7ed88f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "729437c7efd2e8b4c78e",
+    "url": "/portfolio/static/js/main.f94bc9f4.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "6eb5cd8c0cb7b18d79a08a304e83a55d",
+    "url": "/portfolio/static/media/photo.6eb5cd8c.jpg"
+  },
+  {
+    "revision": "62595d43b5e230fd4483d4a25beb150f",
+    "url": "/portfolio/static/media/profile-pic.62595d43.jpg"
+  },
+  {
+    "revision": "2c794e58ad91a9450fe602e989c02267",
+    "url": "/portfolio/static/media/resumeSushant.2c794e58.pdf"
+  }
+]);
